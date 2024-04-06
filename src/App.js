87feaae78a63c459213a0602134c199b3fd6a501
Muralikrishna Patibandla <muralip@iastate.edu>
2024-04-06 18:11:35 -0500

    fixed scrolling issue

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import logo from "./logo.png";
 
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { Products } from "./Products";
 import { Categories } from "./Categories";
 
@@ -60,40 +60,32 @@ const App = () => {
     setProductsCategory(results);
   };
 
-  function mainPage() {
-    return (
-      <div className="flex fixed flex-col">
-        <div className="w-screen bg-gray-100 xl:basis-1/5" style={{ minWidth: "65%" }}>
-          <div className="px-6 py-4">
-            <h1 className="text-3xl mb-2 font-bold text-teal-500"> Product Catalog App </h1>
-            <p className="text-gray-700 text-black">
-              by <b style={{ color: "teal" }}>Gabriel Unser et al</b>
-            </p>
-            <div className="py-3">
-              <input
-                type="search"
-                placeholder="Search"
-                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg
-focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-white-700
+  return (
+    <div className="flex fixed flex-col">
+      <div className="w-screen bg-gray-100 xl:basis-1/5" style={{ minWidth: "65%" }}>
+        <div className="px-6 py-4">
+          <h1 className="text-3xl mb-2 font-bold text-teal-500"> Product Catalog App </h1>
+          <p className="text-gray-700 text-black">
+            by <b style={{ color: "teal" }}>Gabriel Unser solely</b>
+          </p>
+          <div className="py-3">
+            <input
+              type="search"
+              placeholder="Search"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg
+focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700
 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white
 dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                value={query}
-                onChange={handleChange}
-              />
-            </div>
+              value={query}
+              onChange={handleChange}
+            />
           </div>
         </div>
-        <div className="ml-5 p-3 xl:basis-4/5">
-          {console.log("Before render :", Products.length, ProductsCategory.length)}
-          {render_products(ProductsCategory)}
-        </div>
       </div>
-    );
-  }
-
-  return (
-    <div>
-      <mainPage />
+      <div className="ml-5 p-3 xl:basis-4/5">
+        {console.log("Before render :", Products.length, ProductsCategory.length)}
+        {render_products(ProductsCategory)}
+      </div>
     </div>
   );
 };
