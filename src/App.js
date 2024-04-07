29c4bb1bbfcf005b62440729d18acf17d4161c33
Gabriel Unser <gcunser@gmail.com>
2024-04-06 22:33:54 -0500

    making the forms work

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React, { useState } from "react";
 import { Products } from "./Products";
 import "./App.css";
+import Shop from "./Shopping";
+import { useForm } from "react-hook-form";
 
 const CartView = ({ cart, addToCart, removeFromCart, setView }) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const [dataF, setDataF] = useState({});
   const total = cart.reduce((acc, item) => acc + item.price * item.quantity, 0);
 
+  const onSubmit = (data) => {
+    // update hooks
+    setDataF(data);
+  };
   return (
     <div className="container mt-3" style={{ maxHeight: "calc(100vh - 260px)", overflowY: "auto" }}>
       <h2 className="text-3xl font-extrabold tracking-tight text-gray-600 category-title">Cart</h2>
@@ -41,57 +53,80 @@ const CartView = ({ cart, addToCart, removeFromCart, setView }) => {
           </tbody>
         </table>
       </div>
-      
+
       <div className="d-flex justify-content-between align-items-center">
         <p className="total">Total: ${total.toFixed(2)}</p>
       </div>
-      
+
       <hr />
       <br></br>
       <h3 className="text-3xl font-extrabold tracking-tight text-gray-600 category-title">Payment Information</h3>
       <br></br>
       <form>
         <div className="mb-3">
-          <label htmlFor="fullName" className="form-label">Full Name</label>
+          <label htmlFor="fullName" className="form-label">
+            Full Name
+          </label>
           <input type="text" className="form-control" id="fullName" placeholder="Full Name" />
         </div>
         <div className="mb-3">
-          <label htmlFor="email" className="form-label">Email</label>
-          <input type="email" className="form-control" id="email" placeholder="Email" />
+          <label htmlFor="email" className="form-label">
+            Email
+          </label>
+          <input {...register("creditCard", { required: true })} placeholder="Credit Card" />
+          {errors.creditCard && <p>Credit Card is required.</p>}
         </div>
         <div className="mb-3">
-          <label htmlFor="address" className="form-label">Address</label>
-          <input type="text" className="form-control" id="address" placeholder="1234 Main St" />
+          <label htmlFor="address" className="form-label">
+            Address
+          </label>
+          <input {...register("address", { required: true })} placeholder="Address" />
+          {errors.address && <p>Address is required.</p>}
         </div>
         <div className="mb-3">
-          <label htmlFor="address2" className="form-label">Address 2 (Optional)</label>
-          <input type="text" className="form-control" id="address2" placeholder="Apartment, studio, or floor" />
+          <label htmlFor="address2" className="form-label">
+            Address 2 (Optional)
+          </label>
+          <input {...register("address2")} placeholder="Address 2" />
         </div>
         <div className="row g-3">
           <div className="col-md-6">
-            <label htmlFor="city" className="form-label">City</label>
-            <input type="text" className="form-control" id="city" placeholder="City" />
+            <label htmlFor="city" className="form-label">
+              City
+            </label>
+            <input {...register("city", { required: true })} placeholder="City" />
+            {errors.city && <p>City is required.</p>}
           </div>
           <div className="col-md-4">
-            <label htmlFor="state" className="form-label">State</label>
-            <input type="text" className="form-control" id="state" placeholder="State" />
+            <label htmlFor="state" className="form-label">
+              State
+            </label>
+            <input {...register("state", { required: true })} placeholder="State" />
+            {errors.state && <p>State is required.</p>}
           </div>
           <div className="col-md-2">
-            <label htmlFor="zip" className="form-label">Zip</label>
-            <input type="text" className="form-control" id="zip" placeholder="Zip" />
+            <label htmlFor="zip" className="form-label">
+              Zip
+            </label>
+            <input {...register("zip", { required: true })} placeholder="Zip" />
+            {errors.zip && <p>Zip is required.</p>}
+          </div>
+          <div>
+            <button className="btn btn-info rounded-pill px-10" type="button" onClick={() => setView("confirm")}>
+              Confirm purchase
+            </button>
           </div>
         </div>
-        <div className="mb-3">
-          <button className="btn btn-primary mt-3" type="button" onClick={() => setView("confirm")}>
-            Confirm purchase
-          </button> {" "} 
-          <button className="btn btn-secondary mt-3" type="button" onClick={() => setView("products")}>Back to Products</button>
-        </div>
       </form>
+
+      <div>
+        <button className="btn btn-info rounded-pill px-10" type="button" onClick={() => setView("products")}>
+          Back to Products
+        </button>
+      </div>
     </div>
   );
 };
-
 
 const ConfirmView = ({ cart, setView }) => {
   const total = cart.reduce((acc, item) => acc + item.price * item.quantity, 0);
@@ -100,12 +135,7 @@ const ConfirmView = ({ cart, setView }) => {
       <h2>Order summary</h2>
       {cart.map((item, index) => (
         <div key={index}>
-          <img
-            className="img-fluid"
-            src={item.image}
-            width={150}
-            alt={item.title}
-          />
+          <img className="img-fluid" src={item.image} width={150} alt={item.title} />
           {item.title} - ${item.price} x {item.quantity}
         </div>
       ))}
@@ -160,50 +190,31 @@ const App = () => {
   const render_products = (ProductsCategory) => {
     return (
       <div className="category-section">
-        <h2 className="text-3xl font-extrabold tracking-tight text-gray-600 category-title">
-          Products ({ProductsCategory.length})
-        </h2>
-        <div
-          className="m-6 p-3 mt-10 ml-0 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-6 xl:gap-x-10"
-          style={{ maxHeight: "calc(100vh - 300px)", overflowY: "scroll" }}
-        >
+        <h2 className="text-3xl font-extrabold tracking-tight text-gray-600 category-title">Products ({ProductsCategory.length})</h2>
+        <div className="m-6 p-3 mt-10 ml-0 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-6 xl:gap-x-10" style={{ maxHeight: "calc(100vh - 300px)", overflowY: "scroll" }}>
           {/* Loop Products */}
           {ProductsCategory.map((product, index) => (
             <div key={index} className="group relative shadow-lg">
               <div className="min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-60 lg:aspect-none">
-                <img
-                  alt={product.title}
-                  src={product.image}
-                  className="w-full h-full object-center object-cover lg:w-full lg:h-full"
-                />
+                <img alt={product.title} src={product.image} className="w-full h-full object-center object-cover lg:w-full lg:h-full" />
               </div>
               <div className="flex justify-between p-3">
                 <div>
                   <h3 className="text-sm text-gray-700">
-                    <span style={{ fontSize: "16px", fontWeight: "600" }}>
-                      {product.title}
-                    </span>
+                    <span style={{ fontSize: "16px", fontWeight: "600" }}>{product.title}</span>
                   </h3>
 
                   <div>
-                    <button
-                      className="btn btn-outline-secondary btn-sm"
-                      onClick={() => removeFromCart(product.id)}
-                    >
+                    <button className="btn btn-outline-secondary btn-sm" onClick={() => removeFromCart(product.id)}>
                       -
                     </button>
                     <span className="px-2">{howManyofThis(product.id)}</span>
-                    <button
-                      className="btn btn-outline-secondary btn-sm"
-                      onClick={() => addToCart(product)}
-                    >
+                    <button className="btn btn-outline-secondary btn-sm" onClick={() => addToCart(product)}>
                       +
                     </button>
                   </div>
 
-                  <p className="text-sm font-medium text-green-600">
-                    ${product.price}
-                  </p>
+                  <p className="text-sm font-medium text-green-600">${product.price}</p>
                 </div>
               </div>
             </div>
@@ -217,40 +228,25 @@ const App = () => {
     setQuery(e.target.value);
     const results = Products.filter((eachProduct) => {
       if (e.target.value === "") return ProductsCategory;
-      return eachProduct.title
-        .toLowerCase()
-        .includes(e.target.value.toLowerCase());
+      return eachProduct.title.toLowerCase().includes(e.target.value.toLowerCase());
     });
     setProductsCategory(results);
   };
 
   return (
     <div className="flex fixed flex-col">
-      <div
-        className="w-screen bg-gray-100 xl:basis-1/5"
-        style={{ minWidth: "65%" }}
-      >
+      <div className="w-screen bg-gray-100 xl:basis-1/5" style={{ minWidth: "65%" }}>
         <div className="px-6 py-4">
-          <h1 className="text-3xl mb-2 font-bold text-teal-500">
-            Product Catalog App
-          </h1>
+          <h1 className="text-3xl mb-2 font-bold text-teal-500">Product Catalog App</h1>
           <p className="text-gray-700 text-black">
             by <b style={{ color: "teal" }}>Gabriel Unser solely</b>
           </p>
-          <button
-            className="btn btn-info rounded-pill px-10"
-            type="button"
-            onClick={() => setView("cart")}
-          >
+          <button className="btn btn-info rounded-pill px-10" type="button" onClick={() => setView("cart")}>
             Checkout
-          </button> {" "} 
-          <button
-          className="btn btn-info rounded-pill px-10"
-          type="button"
-          onClick={() => setView("products")}
-        >
-          Products
-        </button>
+          </button>{" "}
+          <button className="btn btn-info rounded-pill px-10" type="button" onClick={() => setView("products")}>
+            Products
+          </button>
           <div className="py-3">
             <input
               type="search"
@@ -267,14 +263,7 @@ dark:focus:ring-blue-500 dark:focus:border-blue-500"
       </div>
       <div className="ml-5 p-3 xl:basis-4/5">
         {view === "products" && render_products(ProductsCategory)}
-        {view === "cart" && (
-          <CartView
-            cart={cart}
-            addToCart={addToCart}
-            removeFromCart={removeFromCart}
-            setView={setView}
-          />
-        )}
+        {view === "cart" && <CartView cart={cart} addToCart={addToCart} removeFromCart={removeFromCart} setView={setView} />}
         {view === "confirm" && <ConfirmView cart={cart} setView={setView} />}
       </div>
     </div>
